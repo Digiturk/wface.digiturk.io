35fe86ebfdd9e36ffcc9116658be49bbc63ccb50
mbrn <mehmetbaran@mehmetbaran.net>
2019-09-27 13:38:55 +0300

    0.23.0 version note changes

--- src/pages/versions/index.tsx
+++ src/pages/versions/index.tsx
@@ -12,6 +12,12 @@ export default class VersionsPage extends React.Component<any, any> {
 
 const menuTree = [
   {
+    id: 'v0.24.0',
+    text: 'v0.24.0',
+    path: '0-24-0',
+    mdx: Mdx.V0240Component
+  },
+  {
     id: 'v0.23.0',
     text: 'v0.23.0',
     path: '0-23-0',
@@ -24,15 +30,15 @@ const menuTree = [
     mdx: Mdx.V0220Component
   },
   {
-    id: 'v0.21.0',
-    text: 'v0.21.0',
-    path: '0-21-0',
-    mdx: Mdx.V0210Component
-  },
-  {
     id: 'old-versions',
     text: 'Eski Versiyonlar',
     subNodes: [
+      {
+        id: 'v0.21.0',
+        text: 'v0.21.0',
+        path: '0-21-0',
+        mdx: Mdx.V0210Component
+      },
       {
         id: 'v0.20.0',
         text: 'v0.20.0',
--- src/pages/versions/mdx.jsx
+++ src/pages/versions/mdx.jsx
@@ -16,9 +16,10 @@ import V0170Component from './pages/old-versions/v0.17.0.mdx';
 import V0180Component from './pages/old-versions/v0.18.0.mdx';
 import V0190Component from './pages/old-versions/v0.19.0.mdx';
 import V0200Component from './pages/old-versions/v0.20.0.mdx';
-import V0210Component from './pages/v0.21.0.mdx';
+import V0210Component from './pages/old-versions/v0.21.0.mdx';
 import V0220Component from './pages/v0.22.0.mdx';
 import V0230Component from './pages/v0.23.0.mdx';
+import V0240Component from './pages/v0.24.0.mdx';
 
 const Mdx = {
   V070Component: () => <V070Component/>,
@@ -38,6 +39,7 @@ const Mdx = {
   V0210Component: () => <V0210Component/>,
   V0220Component: () => <V0220Component/>,
   V0230Component: () => <V0230Component/>,
+  V0240Component: () => <V0240Component/>,
 }
 
 export default Mdx
